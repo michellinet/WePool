efd465b1439dfa97bf4cd50909110f2def004024
Michelline Tran <michellinet@procore.com>
2018-02-10 13:23:15 -0800

    Set up Xcode project.

---
+++ WePool/WePool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WePool
+//
+//  Created by Michelline Tran on 2/10/18.
+//  Copyright © 2018 WePoolTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
