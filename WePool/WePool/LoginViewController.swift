69d17de1ea756c8f6196f5f8eb643061f546561c
Michelline Tran <michellinet@procore.com>
2018-02-10 14:46:14 -0800

    Set up login view.

---
+++ WePool/WePool/LoginViewController.swift
@@ -0,0 +1,14 @@
+//
+//  LoginViewController.swift
+//  WePool
+//
+//  Created by Michelline Tran on 2/10/18.
+//  Copyright © 2018 WePoolTeam. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+
+}
